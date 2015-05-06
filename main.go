ec9bbc3807109b37675f9ce054a335c5469140a7
austburn <austburn@gmail.com>
2015-05-06 09:01:06 -0400

    Initial commit, simple seal and open.

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import(
+  "fmt"
+  "golang.org/x/crypto/nacl/box"
+  "crypto/rand"
+)
+
+func main() {
+  publicKey, privateKey, _ := box.GenerateKey(rand.Reader)
+  var nonce [24]byte
+
+  test := []byte{'g', 'o', 'l', 'a', 'n', 'g'}
+  crypt := box.Seal(nil, test, &nonce, publicKey, privateKey)
+  uncrypt, _ := box.Open(nil, crypt, &nonce, publicKey, privateKey)
+
+  fmt.Printf("%s, %s, %s\n", test, crypt, uncrypt)
+}
