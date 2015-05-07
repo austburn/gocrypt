c2f10722d07bac4fa4d46c81bb806c7faac829e3
austburn <austburn@gmail.com>
2015-05-07 15:14:42 -0400

    Use precomputation for speed and ease of use.

--- main.go
+++ main.go
@@ -13,25 +13,29 @@ func main() {
   clientPublicKey, clientPrivateKey, _ := box.GenerateKey(rand.Reader)
   serverPublicKey, serverPrivateKey, _ := box.GenerateKey(rand.Reader)
 
+  var sharedClientKey, sharedServerKey [32]byte
+  box.Precompute(&sharedClientKey, serverPublicKey, clientPrivateKey)
+  box.Precompute(&sharedServerKey, clientPublicKey, serverPrivateKey)
+
   message := []byte{'g', 'o', 'l', 'a', 'n', 'g'}
 
   // Seal with server's public key, aka encrypt it
   // In this scenario, we are about to send a request to the server
   // Encrypt with public, decrypt with private
-  encryptedOnClient := box.Seal(nil, message, &clientNonce, serverPublicKey, clientPrivateKey)
+  encryptedOnClient := box.SealAfterPrecomputation(nil, message, &clientNonce, &sharedClientKey)
   fmt.Printf("The client has encrypted the message: %s as %s\n", message, encryptedOnClient)
 
   // Open with the server's private key, aka decrypt
-  decryptedOnServer, _ := box.Open(nil, encryptedOnClient, &clientNonce, clientPublicKey, serverPrivateKey)
+  decryptedOnServer, _ := box.OpenAfterPrecomputation(nil, encryptedOnClient, &clientNonce, &sharedServerKey)
   fmt.Printf("The server has decrypted the message: %s as %s\n", encryptedOnClient, decryptedOnServer)
 
   // Do whatever with the message we decrypted...
 
   // Encrypt our response, in this case, the message again
-  encryptedOnServer := box.Seal(nil, decryptedOnServer, &clientNonce, clientPublicKey, serverPrivateKey)
+  encryptedOnServer := box.SealAfterPrecomputation(nil, decryptedOnServer, &clientNonce, &sharedServerKey)
   fmt.Printf("The server encrypted the message: %s as %s\n", decryptedOnServer, encryptedOnServer)
 
   // Receive on the client
-  decryptedOnClient, _ := box.Open(nil, encryptedOnServer, &clientNonce, serverPublicKey, clientPrivateKey)
+  decryptedOnClient, _ := box.OpenAfterPrecomputation(nil, encryptedOnServer, &clientNonce, &sharedClientKey)
   fmt.Printf("The client decrypted the message: %s as %s\n", encryptedOnServer, decryptedOnClient)
 }
